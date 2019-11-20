4cd92725b16976e4fa01cc1398c89771ccefa12e
pcybxine <sine.narisa@gmail.com>
2019-11-20 17:40:22 +0700

    Change 2 place in program1

--- bloodgroup.c
+++ bloodgroup.c
@@ -5,7 +5,7 @@ void main()
   int i;
   do
   {
-    printf("What is your Name? ");
+    printf("Hey Guys, What is your Name? ");
     scanf("%s", Name);
     do
     {
@@ -23,7 +23,7 @@ void main()
                             strcmp(G, "O") !=0 );
     if (strcmp(G,"A") ==0)
     {
-      printf("%s, A. Hey, you can give blood to: A, AB.\n", Name);
+      printf("%s, A. Hey, you can give blood to: A, AB. Woww that's amaze\n", Name);
       printf("  You can receive blood from: A, O.\n");
     }
     else if (strcmp(G, "B") ==0)
